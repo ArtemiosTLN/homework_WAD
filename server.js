3e44188685fe314b7d3c2bcc710933d6fc9afefe
goddess of the hunt <artemi.sotskov@gmail.com>
2023-12-13 15:48:54 +0200

    database support added, not implemented properly yet

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-import express from 'express';
-import pool from './database.js';
-import cors from 'cors';
+import express from 'express'
+import pool from './database.js'
+import cors from 'cors'
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
-
-// The express.json() function is a built-in middleware function in Express.
-// It parses incoming requests with JSON payloads and is based on body-parser.
+app.use(cors())
 app.use(express.json());
 
-    //  Handling HTTP requests code will go here
 
 app.get('/', (req, res) => {
     // res.send can be used to send text and HTML snippets
@@ -21,6 +17,63 @@ app.get('/', (req, res) => {
     However, it takes the absolute not the relative path to the file. Therefore, you need to specify what is the root directory.
     To avoid this confusion, you can use  "__dirname"*/
     res.sendFile('./index.html', root);
+});
+
+// add post
+app.post('/api/posts/', async(req, res) => {
+    try {
+        console.log("a post request has arrived");
+        const post = req.body;
+        const newpost = await pool.query(
+            "INSERT INTO posts(date, author, content, image) values ($1, $2, $3, $4)    RETURNING*", [post.date, post.author, post.content, post.image]
+        );
+        res.json(newpost);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// get all posts
+app.get('/api/posts', async(req, res) => {
+    try {
+        console.log("get posts request has arrived");
+        const posts = await pool.query(
+            "SELECT * FROM posts"
+        );
+        res.json(posts.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// update post
+app.put('/api/posts/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        const post = req.body;
+        console.log("update request has arrived");
+        const updatepost = await pool.query(
+            "UPDATE posts SET (date, author, content, urllink) = ($2, $3, $4, $5) WHERE id = $1", [id, post.date, post.author, post.content, post.image]
+        );
+        res.json(updatepost);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// delete post
+app.delete('/api/posts/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+        //const post = req.body; // we do not need a body for a delete request
+        console.log("delete a post request has arrived");
+        const deletepost = await pool.query(
+            "DELETE FROM posts WHERE id = $1", [id]
+        );
+        res.json(deletepost);
+    } catch (err) {
+        console.error(err.message);
+    }
 });
 
 app.listen(port, () => {
--- src/js/footer.js
+++ src/js/footer.js
@@ -1,11 +1,11 @@
 import '@/assets/style.css'
-import App from "@/components/vue_footer.vue";
+import App from "@/components/footer.vue";
 //import store from "./store/store.js";
 import { createApp } from 'vue'
 import vue_footer from '@/components/vue_footer.vue'
 
 const footer = createApp(App)
 
-footer.component('vue-footer', vue_footer)
+footer.component('footer', footer)
 
 footer.mount('#footer')
